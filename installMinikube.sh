9ae35f8035ccab64886b09c8be5652628e113d8c
zeehasham <48657324+zeejafri@users.noreply.github.com>
2024-11-25 00:32:57 +0500

    Update installMinikube.sh

--- installMinikube.sh
+++ installMinikube.sh
@@ -1,6 +1,9 @@
 echo "***** Installing Minikube ****"
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
+#curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+#sudo dpkg -i minikube_latest_amd64.deb
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
 minikube start -p multinode-demo
 echo "***** Adding 2 worker nodes to the Cluster *****"
 minikube node add --worker -p multinode-demo
